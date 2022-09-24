7bb826ab9f2ab5743f815a21d68a96bc273293d2
chidijoe84 <anigbo.joel@gmail.com>
2022-09-24 23:43:18 +0100

    reactjs-website

---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+import Bill from './Bill';
+import Business from './Business';
+import Button from './Button';
+import CardDeal from './CardDeal';
+import Clients from './Clients';
+import CTA from './CTA';
+import FeedBack from './FeedBackCard';
+import Footer from './Footer';
+import GetStarted from './GetStarted';
+import Hero from './Hero';
+import Navbar from './Navbar';
+import Stats from './Stats';
+import Testimonials from './Testimonials';
+
+export {
+
+    Bill,
+    Business,
+    Button,
+    CardDeal,
+    Clients,
+    CTA,
+    FeedBack,
+    Footer,
+    GetStarted,
+    Hero,
+    Navbar,
+    Stats,
+    Testimonials
+};
